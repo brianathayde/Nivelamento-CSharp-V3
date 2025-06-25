274b7e10744d9c7c7f27a5604f463775cc5ddcde
Brian Athayde <brian_athayde@hotmail.com>
2025-06-25 19:07:50 -0300

    questão 1 finalizada

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -3,7 +3,49 @@
 namespace Questao1
 {
     class ContaBancaria {
+        private const double TaxaSaque = 3.5;
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
 
-       
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = depositoInicial;
+        }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+
+        public void Deposito(double quantia)
+        {
+            if (quantia > 0)
+            {
+                Saldo += quantia;
+            }
+        }
+
+        public void Saque(double quantia)
+        {
+            if (quantia > 0)
+            {
+                Saldo -= quantia + TaxaSaque;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
